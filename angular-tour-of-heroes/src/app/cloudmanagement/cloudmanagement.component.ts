0ceba33df2ea547a77ca42260599187297786f4a
Your Name <you@example.com>
2020-11-18 21:46:26 -0800

    googleDrive Login cloudmanagement-1

--- angular-tour-of-heroes/src/app/cloudmanagement/cloudmanagement.component.ts
+++ angular-tour-of-heroes/src/app/cloudmanagement/cloudmanagement.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FilterService } from '../filter.service';
-
+let filterServiceGlobal: FilterService
+let holdClientEmail = []
+let getEmailValue = false
 @Component({
   selector: 'app-cloudmanagement',
   templateUrl: './cloudmanagement.component.html',
@@ -19,7 +21,8 @@ export class CloudmanagementComponent {
   googleDriveForm = false;
   oneDriveForm = false;
   boxForm = false;
-
+  gdEmail:string;
+  
   dbAccount = {
     "username": "",
     "password": ""
@@ -71,4 +74,51 @@ export class CloudmanagementComponent {
   linkAccount(): void {
     // This should link the account if the passed in username and password are accurate
   }
+  async googleDriveInit(){
+    //this.googleDriveForm = true
+    this.googleImplementCallBack()
+    setTimeout(() => this.getClientEmail(),40000 )  
+  }
+  clientEmailValue(v: string) {
+    console.log('set is called ')
+    this.gdEmail = v;
+    console.log('the value from set ' + v)
+    console.log('the value from set2 ' + this.gdEmail)
+  }
+  googleImplementCallBack(){
+   gapi.load('client:auth2', initClient) 
+  }
+  getClientEmail(){
+   this.clientEmailValue(holdClientEmail[0])
+  }
 }
+function initClient(){
+  gapi.client
+  .init({
+    apiKey: 'AIzaSyCoO79P9OtAYVmr6PUSNqRF69PmAMwyuiA',
+    discoveryDocs: [
+      'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
+    ],
+    clientId:
+      '160810936655-90na0qia4bkvqrsljk6acttn60tru758.apps.googleusercontent.com',
+    scope:
+      'profile email https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.appdata',
+  })
+  .then(function() {
+    console.log("client initialized...")
+    getClientdata()
+    })
+ }
+ function getClientdata() {
+    let showClient = gapi.auth2.getAuthInstance();
+      return showClient.signIn({prompt: 'consent' })
+        .then((googleUser: gapi.auth2.GoogleUser) => {
+          let clientaccessToken = googleUser.getAuthResponse().access_token
+          let clientEmail = googleUser.getBasicProfile().getEmail();
+          let InstantiateClient = showClient.isSignedIn.get();
+          getEmailValue = true
+          holdClientEmail.push(clientEmail,clientaccessToken)
+          return holdClientEmail
+      })
+}
+
